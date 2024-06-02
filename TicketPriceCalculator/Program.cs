a7563969572993c09099162d5f6e32831d6cc7ca
qiitoboy <ransfordtakyi149@gmail.com>
2024-06-02 02:48:58 +0000

    Add Ticket Price Calculator application

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+            int ticketPrice;
+
+            if (age <= 12 || age >= 65)
+            {
+                ticketPrice = 7;
+            }
+            else
+            {
+                ticketPrice = 10;
+            }
+
+            Console.WriteLine("Your ticket price is: GHC" + ticketPrice);
+        }
+    }
+}
